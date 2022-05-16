0dbb6d429406538bc5a5d4b8dbff91bf8aef4080
9956799@gmail.com <Qqq_12345>
2022-05-16 20:23:52 +0300

    Branch22 4 (after deleted branch locally)

--- src/main/java/com/example/awsuploadtest/AwsUploadTestApplication.java
+++ src/main/java/com/example/awsuploadtest/AwsUploadTestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // changes ----------- 2222222222222 333333 (after deleted branch)
+// 444444 (after deleted branch locally)
 
 @SpringBootApplication
 public class AwsUploadTestApplication {
