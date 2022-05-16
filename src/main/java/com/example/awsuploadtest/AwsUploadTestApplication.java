7e5416c43beb3a09327c9d18755b47992fb86c66
9956799@gmail.com <Qqq_12345>
2022-05-16 19:54:43 +0300

    Branch22

--- src/main/java/com/example/awsuploadtest/AwsUploadTestApplication.java
+++ src/main/java/com/example/awsuploadtest/AwsUploadTestApplication.java
@@ -3,6 +3,8 @@ package com.example.awsuploadtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// changes-----------
+
 @SpringBootApplication
 public class AwsUploadTestApplication {
 
