c210f50e25fd23b22f79939bea9e29a7950ab8c8
maxlrose2 <Qqq_12345>
2022-05-19 15:15:45 +0300

    main update 2AA

---
+++ src/main/java/com/example/awsuploadtest/AwsUploadTestApplication.java
@@ -0,0 +1,20 @@
+package com.example.awsuploadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// changes ----------- 2222222222222 333333 (after deleted branch)
+// br3 (after deleted branch locally)
+
+// Fork 2
+
+// main update 2A
+
+@SpringBootApplication
+public class AwsUploadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsUploadTestApplication.class, args);
+	}
+
+}
